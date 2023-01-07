a86e371d844464c01e135f42ff031c7983660c8a
Rod97139 <fayaflame@gmail.com>
2023-01-07 22:53:13 +0100

    schema

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
 import { createSchema, createYoga } from 'graphql-yoga'
 import { createServer } from 'node:http'
 
+const typeDef = "src/schema/schema.graphql"
+
 const yoga = createYoga({
     
     // définir le schéma de GraphQL
   schema: createSchema({
     // Notre contrat ce que nous offrons à notre serveur GraphQL
-    typeDefs: /* GraphQL */ `
-      type Query {
-        hello(name: String): String!
+    typeDefs: /* GraphQL */`
+
+      enum todoStatusEnum {
+        WITING
+        IN_PROGRESS
+        CANCLED
+        DONE
       }
-    `,
+    
+      type todo {
+        id: ID!
+        name: String!
+        content: String!
+        status: todoStatusEnum!
+      }
+
+      type Query {
+      hello(name: String): String!
+        }`,
 
     // implémentation de notre contrat
     resolvers: {
       Query: {
-        hello: (_,{name}) => `Hello ${name || "La mifa!" }`
+        hello: (_,{name}) => `Hello ${name || "World!" }`
       }
     }
   })
